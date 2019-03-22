12897d361d8d13125857ad48b9eab0c68b297fae
tschizas <thomas.schizas@gmail.com>
2019-03-22 14:08:29 +0000

    First commit

---
+++ SimpleWeb/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  re.send('Hello!';)
+});
+
+app.listen(8080, () => {
+  console.log('Listening at 8080');
+});
